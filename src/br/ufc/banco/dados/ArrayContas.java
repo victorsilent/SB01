64874926414dbced7d8d2692337f26363421f13f
Stenio Wagner <stenio.wagner1@gmail.com>
2015-12-05 16:02:24 -0300

    [#2] Alterar o limite de ArrayContas para ilimitado

--- src/br/ufc/banco/dados/ArrayContas.java
+++ src/br/ufc/banco/dados/ArrayContas.java
@@ -1,26 +1,24 @@
 package br.ufc.banco.dados;
 
+import java.util.ArrayList;
+
 import br.ufc.banco.conta.ContaAbstrata;
 import br.ufc.banco.dados.excecoes.CEException;
 import br.ufc.banco.dados.excecoes.CIException;
 
 public class ArrayContas implements IRepositorioContas {
 
-	private ContaAbstrata[] contas = null;
-	private int indice = 0;
+	private ArrayList<ContaAbstrata> contas;
 
 	public ArrayContas() {
-		this.contas = new ContaAbstrata[100];
+		this.contas = new ArrayList<>();
 	}
 
 	public void apagar(String numero) throws CIException {
 		if (this.procurar(numero) != null) {
-			for (int i = 0; i < indice; i++) {
-				if (contas[i] != null && contas[i].obterNumero().equals(numero)) {
-					for (int j = i; j < indice - 1; j++) {
-						contas[j] = contas[j + 1];
-					}
-					contas[indice++] = null;
+			for (int i = 0; i < contas.size(); i++) {
+				if (contas.get(i) != null && contas.get(i).obterNumero().equals(numero)) {
+					contas.remove(i);
 				}
 			}
 		} else {
@@ -30,32 +28,31 @@ public class ArrayContas implements IRepositorioContas {
 
 	public void inserir(ContaAbstrata conta) throws CEException {
 		if (this.procurar(conta.obterNumero()) != null) {
-			this.contas[indice++] = conta;
+			this.contas.add(conta);
 		} else {
 			throw new CEException(conta.obterNumero());
 		}
 	}
 
 	public ContaAbstrata[] listar() {
-		ContaAbstrata[] lista = null;
-		if (indice > 0) {
-			lista = new ContaAbstrata[indice];
-			for (int i = 0; i < indice; i++) {
-				lista[i] = this.contas[i];
-			}
+		ContaAbstrata[] lista = new ContaAbstrata[contas.size()];
+	
+		for (int i = 0; i < contas.size(); i++) {
+			lista[i] = this.contas.get(i);
 		}
+		
 		return lista;
 	}
 
 	public int numeroContas() {
-		return indice;
+		return contas.size();
 	}
 
 	public ContaAbstrata procurar(String numero) {
-		if (this.indice > 0) {
-			for (int i = 0; i < indice; i++) {
-				if (contas[i] != null && contas[i].obterNumero().equals(numero)) {
-					return contas[i];
+		if (this.contas.size() > 0) {
+			for (int i = 0; i < contas.size(); i++) {
+				if (contas.get(i) != null && contas.get(i).obterNumero().equals(numero)) {
+					return contas.get(i);
 				}
 			}
 		}
